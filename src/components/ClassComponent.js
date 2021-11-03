8b49446efa0a82637b7eb519bffc1a5776f5fd2e
João Victor A <jvas1999@gmail.com>
2021-11-03 16:20:22 -0300

    up das atv

---
+++ src/components/ClassComponent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class ClassComponent extends React.Component {
+    render(){
+        return(
+
+            <div className="main">
+                <h1>Olá Mundo React</h1>
+                <p>Esse é o meu primeiro componente de Classe</p>
+            </div>
+
+        )
+    }
+}
+export default ClassComponent;
